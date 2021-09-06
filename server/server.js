142b18f73ad9e7b47f1c65dd8c6a9fa6370ccc75
kueben96 <kuebra.tokuc@haw-hamburg.de>
2021-09-06 10:40:20 +0300

    add node engine for heroku deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const mongoSanitize  = require('express-mongo-sanitize');
 const routes = require('./routes')
 const passport = require('passport');
 const {jwtStrategy} = require('./middleware/passport')
-const {handleError, convertToApiError } = require('./middleware/apiError')
+const {handleError, convertToApiError } = require('./middleware/apiError');
+const { route } = require('./routes');
 //mongodb+srv://admin:<password>@cluster0.sadmj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`
@@ -40,6 +41,16 @@ app.use(convertToApiError);
 app.use((err,req,res,next)=>{
     handleError(err,res)
 })
+
+// last step for deploy:
+
+route.use(express.static('client/build'))
+if(process.env.NODE_ENV==='production'){
+    const path = require('path');
+    app.get('/*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    });
+}
 
 // starts the server
 const port = process.env.PORT || 3001;
